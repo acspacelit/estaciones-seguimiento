76847d9100dd4df545bedff2e00988730957787b
acspacelit <161153689+acspacelit@users.noreply.github.com>
2024-03-01 13:28:48 +0000

    swe

--- pages/0_Animation_Demo.py
+++ pages/0_Animation_Demo.py
@@ -1,84 +1,299 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from typing import Any
-
-import numpy as np
-
 import streamlit as st
-from streamlit.hello.utils import show_code
+import pandas as pd
+import altair as alt
+from local_components import card_container
+import streamlit_shadcn_ui as ui
 
+# Configuración inicial de la página
+def setup_page():
+    st.set_page_config(page_title="Análisis de Eficiencia Operativa", page_icon="📊")
 
-def animation_demo() -> None:
+# Función para cargar los datos desde la URL
+def load_data_from_url(url):
+    try:
+        return pd.read_csv(url, header=0)
+    except Exception as e:
+        st.error("Error al cargar los datos: " + str(e))
+        return None
 
-    # Interactive Streamlit elements, like these sliders, return their value.
-    # This gives you an extremely simple interaction model.
-    iterations = st.sidebar.slider("Level of detail", 2, 20, 10, 1)
-    separation = st.sidebar.slider("Separation", 0.7, 2.0, 0.7885)
+# Función para convertir valores a numéricos
+def to_numeric(column):
+    return pd.to_numeric(column.str.replace(',', '.'), errors='coerce')
 
-    # Non-interactive elements return a placeholder to their location
-    # in the app. Here we're storing progress_bar to update it later.
-    progress_bar = st.sidebar.progress(0)
+# Función para crear los widgets de interactividad
+def create_interactive_widgets(data):
+    years = data['AÑO'].dropna().astype(int)
+    min_year, max_year = int(years.min()), int(years.max())
+    selected_years = st.slider('Selecciona el rango de años:', min_year, max_year, (min_year, max_year))
 
-    # These two elements will be filled in later, so we create a placeholder
-    # for them using st.empty()
-    frame_text = st.sidebar.empty()
-    image = st.empty()
+    all_stations = ['Todas'] + list(data['Estaciones'].dropna().unique())
+    selected_station = st.selectbox('Selecciona una Estación', all_stations)
 
-    m, n, s = 960, 640, 400
-    x = np.linspace(-m / s, m / s, num=m).reshape((1, m))
-    y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))
+    all_countries = ['Todos'] + list(data['Pais'].dropna().unique())
+    selected_countries = st.multiselect('Selecciona Países', all_countries, default='Todos')
 
-    for frame_num, a in enumerate(np.linspace(0.0, 4 * np.pi, 100)):
-        # Here were setting value for these two elements.
-        progress_bar.progress(frame_num)
-        frame_text.text("Frame %i/100" % (frame_num + 1))
+    return selected_years, selected_station, selected_countries
 
-        # Performing some fractal wizardry.
-        c = separation * np.exp(1j * a)
-        Z = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
-        C = np.full((n, m), c)
-        M: Any = np.full((n, m), True, dtype=bool)
-        N = np.zeros((n, m))
+# Función para filtrar el DataFrame según la selección del usuario
+def filter_data(data, selected_years, selected_station, selected_countries):
+    filtered_df = data[
+        (data['AÑO'] >= selected_years[0]) &
+        (data['AÑO'] <= selected_years[1])
+    ]
 
-        for i in range(iterations):
-            Z[M] = Z[M] * Z[M] + C[M]
-            M[np.abs(Z) > 2] = False
-            N[M] = i
+    if selected_station != 'Todas':
+        filtered_df = filtered_df[filtered_df['Estaciones'] == selected_station]
 
-        # Update the image placeholder by calling the image() function on it.
-        image.image(1.0 - (N / N.max()), use_column_width=True)
+    if 'Todos' not in selected_countries:
+        filtered_df = filtered_df[filtered_df['Pais'].isin(selected_countries)]
+    return filtered_df
 
-    # We clear elements by calling empty on them.
-    progress_bar.empty()
-    frame_text.empty()
+# Función para crear métricas y gráficos basada en los datos filtrados
+def create_metrics_and_charts(filtered_df):
+    st.header("Análisis de la Eficiencia Operativa")
+    
+    # Cálculos previos de tus métricas
+    average_kpi = filtered_df['KPI'].mean()
+    unique_operation_count = filtered_df['IDEtapa'].nunique()
+    total_stations = ((filtered_df['AporteFONPLATAVigente'].sum())/1000000).round(2)
 
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
+    Infraestructura = filtered_df[filtered_df['SEC'] == 'INF']
+    inf_operation = len(Infraestructura)
 
+    Social = filtered_df[filtered_df['SEC'] == 'SOC']
+    soc_operation = len(Social)
 
-st.set_page_config(page_title="Animation Demo", page_icon="📹")
-st.markdown("# Animation Demo")
-st.sidebar.header("Animation Demo")
-st.write(
-    """This app shows how you can use Streamlit to build cool animations.
-It displays an animated fractal based on the the Julia Set. Use the slider
-to tune different parameters."""
-)
+    Productivo = filtered_df[filtered_df['SEC'] == 'PRO']
+    pro_operation = len(Productivo)
 
-animation_demo()
+    # Primera fila de tarjetas
+    row1_cols = st.columns(3)  # Esto crea tres columnas para la primera fila
 
-show_code(animation_demo)
+    with row1_cols[0]:
+        ui.metric_card(
+            title="Tiempo Promedio",
+            content=f"{average_kpi:.2f} Meses",
+            description="Promedio de KPI",
+            key="card_kpi",
+        )
+
+    with row1_cols[1]:
+        ui.metric_card(
+            title="Proyectos Totales",
+            content=str(unique_operation_count),
+            description="Total de proyectos únicos",
+            key="card_projects",
+        )
+
+    with row1_cols[2]:
+        ui.metric_card(
+            title="Aporte Fonplata",
+            content=f"${total_stations}M",
+            description="En millones de dólares",
+            key="card_aporte",
+        )
+
+    # Segunda fila de tarjetas
+    row2_cols = st.columns(3)  # Esto crea tres columnas para la segunda fila
+
+    with row2_cols[0]:
+        ui.metric_card(
+            title="Infraestructura",
+            content=str(inf_operation),
+            description="Proyectos de Infraestructura",
+            key="card_infra",
+        )
+
+    with row2_cols[1]:
+        ui.metric_card(
+            title="Socio-Económicos",
+            content=str(soc_operation),
+            description="Proyectos Socio-Económicos",
+            key="card_social",
+        )
+
+    with row2_cols[2]:
+        ui.metric_card(
+            title="Productivos",
+            content=str(pro_operation),
+            description="Proyectos Productivos",
+            key="card_productivo",
+        )
+
+# Función principal de la aplicación
+def main():
+    setup_page()
+
+    # Título de la aplicación
+    st.title('Dashboard de Streamlit')
+
+    # Carga los datos
+    data = load_data_from_url("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3uglhp1iEb6nz_Rjh6SnKyt0GqaAxOwGIqsQEdgcwJfrSP2wOZqFfrIjKL3KfsLzi4sSq2HJ3nkAt/pub?gid=0&single=true&output=csv")
+
+    # Aplicar filtros al DataFrame
+    filtered_df = filter_data(data)
+
+    # Mostrar gráficos y métricas
+    show_metrics(filtered_df)
+    create_bar_chart(filtered_df)
+    create_pie_chart(filtered_df)
+
+# Función para filtrar datos
+def filter_data(data):
+    years = data['AÑO'].dropna().astype(int)
+    min_year, max_year = int(years.min()), int(years.max())
+    selected_years = st.slider('Selecciona el rango de años:', min_year, max_year, (min_year, max_year))
+
+    all_stations = ['Todas'] + list(data['Estaciones'].dropna().unique())
+    selected_station = st.selectbox('Selecciona una Estación', all_stations)
+
+    all_countries = ['Todos'] + list(data['Pais'].dropna().unique())
+    selected_countries = st.multiselect('Selecciona Países', all_countries, default='Todos')
+
+    filtered_df = data[
+        (data['AÑO'] >= selected_years[0]) &
+        (data['AÑO'] <= selected_years[1])
+    ]
+
+    if selected_station != 'Todas':
+        filtered_df = filtered_df[filtered_df['Estaciones'] == selected_station]
+
+    if 'Todos' not in selected_countries:
+        filtered_df = filtered_df[filtered_df['Pais'].isin(selected_countries)]
+
+    return filtered_df
+
+# Función para mostrar métricas
+def show_metrics(filtered_df):
+    st.header("Análisis de la Eficiencia Operativa")
+
+    average_kpi = filtered_df['KPI'].mean()
+    unique_operation_count = filtered_df['IDEtapa'].nunique()
+    total_stations = ((filtered_df['AporteFONPLATAVigente'].sum())/1000000).round(2)
+
+    Infraestructura = filtered_df[filtered_df['SEC'] == 'INF']
+    inf_operation = len(Infraestructura)
+
+    Social = filtered_df[filtered_df['SEC'] == 'SOC']
+    soc_operation = len(Social)
+
+    Productivo = filtered_df[filtered_df['SEC'] == 'PRO']
+    pro_operation = len(Productivo)
+
+    row1_cols = st.columns(3)
+
+    with row1_cols[0]:
+        ui.metric_card(
+            title="Tiempo Promedio",
+            content=f"{average_kpi:.2f} Meses",
+            description="Promedio de KPI",
+            key="card_kpi",
+        )
+
+    with row1_cols[1]:
+        ui.metric_card(
+            title="Proyectos Totales",
+            content=str(unique_operation_count),
+            description="Total de proyectos únicos",
+            key="card_projects",
+        )
+
+    with row1_cols[2]:
+        ui.metric_card(
+            title="Aporte Fonplata",
+            content=f"${total_stations}M",
+            description="En millones de dólares",
+            key="card_aporte",
+        )
+
+    row2_cols = st.columns(3)
+
+    with row2_cols[0]:
+        ui.metric_card(
+            title="Infraestructura",
+            content=str(inf_operation),
+            description="Proyectos de Infraestructura",
+            key="card_infra",
+        )
+
+    with row2_cols[1]:
+        ui.metric_card(
+            title="Socio-Económicos",
+            content=str(soc_operation),
+            description="Proyectos Socio-Económicos",
+            key="card_social",
+        )
+
+    with row2_cols[2]:
+        ui.metric_card(
+            title="Productivos",
+            content=str(pro_operation),
+            description="Proyectos Productivos",
+            key="card_productivo",
+        )
+
+# Función para crear gráfico de barras
+def create_bar_chart(filtered_df):
+    kpi_avg_by_country_station = filtered_df.groupby(['Pais', 'Estaciones'])['KPI'].mean().reset_index()
+
+    color_scheme = {
+        "Aprobacion": "lightgreen",
+        "Vigencia": "skyblue",
+        "PrimerDesembolso": "salmon",
+        "Elegibilidad": "gold"
+    }
+
+    bar_chart = alt.Chart(kpi_avg_by_country_station).mark_bar().encode(
+        x='Pais:N',
+        y=alt.Y('sum(KPI):Q', stack='zero', title='KPI Promedio'),
+        color=alt.Color('Estaciones:N', scale=alt.Scale(domain=list(color_scheme.keys()), range=list(color_scheme.values()))),
+        tooltip=['Pais', 'Estaciones', 'KPI']
+    )
+
+    text_chart = alt.Chart(kpi_avg_by_country_station).mark_text(
+        align='center',
+        baseline='middle',
+        color='black',
+    ).encode(
+        x='Pais:N',
+        y=alt.Y('sum(KPI):Q', stack='zero', title=''),
+        text=alt.Text('sum(KPI):Q', format='.2f'),
+        color=alt.value('black')
+    )
+
+    final_chart = (bar_chart + text_chart).properties(
+        width=600,
+        height=400,
+        title='KPI Promedio por País y Estación'
+    )
+
+    st.altair_chart(final_chart)
+
+# Función para crear gráfico de pastel
+def create_pie_chart(filtered_df):
+    country_colors = {
+        "Argentina": "#36A9E1",
+        "Bolivia": "#F39200",
+        "Brasil": "#009640",
+        "Paraguay": "#E30613",
+        "Uruguay": "#27348B"
+    }
+
+    pie_chart = alt.Chart(filtered_df).transform_aggregate(
+        total='sum(AporteFONPLATAVigente)',
+        groupby=['Pais']
+    ).transform_calculate(
+        porcentaje='datum.total / sum(datum.total) over ()'
+    ).mark_arc().encode(
+        theta=alt.Theta(field='porcentaje', type='quantitative', stack=True),
+        color=alt.Color('Pais:N', scale=alt.Scale(domain=list(country_colors.keys()), range=list(country_colors.values())), legend=alt.Legend(title="País")),
+        tooltip=[alt.Tooltip('Pais:N'), alt.Tooltip('total:Q', title='Aporte FONPLATA Vigente')],
+        text=alt.Text('Pais:N')
+    ).properties(
+        title='Distribución de Aportes FONPLATA Vigentes por País'
+    )
+
+    st.altair_chart(pie_chart, use_container_width=True)
+
+if __name__ == "__main__":
+    main()
--- pages/1_Plotting_Demo.py
+++ pages/1_Plotting_Demo.py
@@ -1,56 +1,226 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import time
-
-import numpy as np
-
 import streamlit as st
-from streamlit.hello.utils import show_code
+import pandas as pd
+import re
+from datetime import datetime
+import io
+import seaborn as sns
+import matplotlib.pyplot as plt
+import streamlit_shadcn_ui as ui
+from local_components import card_container
+import altair as alt
+
+# Configuración inicial de la página
+st.set_page_config(page_title="Análisis de Eficiencia Operativa", page_icon="📊")
+
+# URLs de las hojas de Google Sheets
+estaciones = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3uglhp1iEb6nz_Rjh6SnKyt0GqaAxOwGIqsQEdgcwJfrSP2wOZqFfrIjKL3KfsLzi4sSq2HJ3nkAt/pub?gid=0&single=true&output=csv"
+
+# Función para cargar los datos desde la URL
+def load_data_from_url(url):
+    try:
+        return pd.read_csv(url, header=0)
+    except Exception as e:
+        st.error("Error al cargar los datos: " + str(e))
+        return None
+    
+def get_year_for_operation(date):
+    return date.year if pd.notnull(date) else None
+
+# Título de la aplicación
+st.title('Dashboard de Streamlit')
+
+# Carga los datos
+data = load_data_from_url(estaciones)
+
+def to_numeric(column):
+    # Remove commas and convert to numeric, coercing errors to NaN
+    return pd.to_numeric(column.str.replace(',', '.'), errors='coerce')
+
+# Assume 'KPI' is the column you're having trouble with
+data['KPI'] = to_numeric(data['KPI'])
+data['AporteFONPLATAVigente'] = to_numeric(data['AporteFONPLATAVigente'])
+
+# Widgets para interactividad
+years = data['AÑO'].dropna().astype(int)
+min_year, max_year = int(years.min()), int(years.max())
+selected_years = st.slider('Selecciona el rango de años:', min_year, max_year, (min_year, max_year))
+
+all_stations = ['Todas'] + list(data['Estaciones'].dropna().unique())
+selected_station = st.selectbox('Selecciona una Estación', all_stations)
+
+# Filtro por país con opción "Todos"
+all_countries = ['Todos'] + list(data['Pais'].dropna().unique())
+selected_countries = st.multiselect('Selecciona Países', all_countries, default='Todos')
+
+# Aplicar filtros al DataFrame
+filtered_df = data[
+    (data['AÑO'] >= selected_years[0]) &
+    (data['AÑO'] <= selected_years[1])
+        ]
+
+if selected_station != 'Todas':
+    filtered_df = filtered_df[filtered_df['Estaciones'] == selected_station]
+
+        # Modificado para manejar múltiples selecciones de países
+    if 'Todos' not in selected_countries:
+        filtered_df = filtered_df[filtered_df['Pais'].isin(selected_countries)]
 
 
-def plotting_demo():
-    progress_bar = st.sidebar.progress(0)
-    status_text = st.sidebar.empty()
-    last_rows = np.random.randn(1, 1)
-    chart = st.line_chart(last_rows)
+st.header("         Análisis de la Eficiencia Operativa")
+figsize = (7, 5)  # Definir el tamaño de la figura para los gráficos
 
-    for i in range(1, 101):
-        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-        status_text.text("%i%% Complete" % i)
-        chart.add_rows(new_rows)
-        progress_bar.progress(i)
-        last_rows = new_rows
-        time.sleep(0.05)
+# Cálculos previos de tus métricas
+average_kpi = filtered_df['KPI'].mean()
+unique_operation_count = filtered_df['IDEtapa'].nunique()
+total_stations = ((filtered_df['AporteFONPLATAVigente'].sum())/1000000).round(2)
 
-    progress_bar.empty()
+Infraestructura = filtered_df[filtered_df['SEC'] == 'INF']
+inf_operation = len(Infraestructura)
 
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
+Social = filtered_df[filtered_df['SEC'] == 'SOC']
+soc_operation = len(Social)
 
+Productivo = filtered_df[filtered_df['SEC'] == 'PRO']
+pro_operation = len(Productivo)
 
-st.set_page_config(page_title="Plotting Demo", page_icon="📈")
-st.markdown("# Plotting Demo")
-st.sidebar.header("Plotting Demo")
-st.write(
-    """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
+# Primera fila de tarjetas
+row1_cols = st.columns(3)  # Esto crea tres columnas para la primera fila
+
+with row1_cols[0]:
+    ui.metric_card(
+        title="Tiempo Promedio",
+        content=f"{average_kpi:.2f} Meses",
+        description="Promedio de KPI",
+        key="card_kpi",
+    )
+
+with row1_cols[1]:
+    ui.metric_card(
+        title="Proyectos Totales",
+        content=str(unique_operation_count),
+        description="Total de proyectos únicos",
+        key="card_projects",
+    )
+
+with row1_cols[2]:
+    ui.metric_card(
+        title="Aporte Fonplata",
+        content=f"${total_stations}M",
+        description="En millones de dólares",
+        key="card_aporte",
+    )
+
+# Segunda fila de tarjetas
+row2_cols = st.columns(3)  # Esto crea tres columnas para la segunda fila
+
+with row2_cols[0]:
+    ui.metric_card(
+        title="Infraestructura",
+        content=str(inf_operation),
+        description="Proyectos de Infraestructura",
+        key="card_infra",
+    )
+
+with row2_cols[1]:
+    ui.metric_card(
+        title="Socio-Económicos",
+        content=str(soc_operation),
+        description="Proyectos Socio-Económicos",
+        key="card_social",
+    )
+
+with row2_cols[2]:
+    ui.metric_card(
+        title="Productivos",
+        content=str(pro_operation),
+        description="Proyectos Productivos",
+        key="card_productivo",
+    )
+
+# Pivotear el DataFrame para obtener el KPI promedio por país y año
+kpi_pivot_df = filtered_df.pivot_table(values='KPI', index='Pais', columns='AÑO', aggfunc='mean')
+
+        # Redondear todos los valores numéricos a dos decimales
+kpi_pivot_df = kpi_pivot_df.round(2)
+
+        # Opción para reemplazar los valores None/NaN con un string vacío
+kpi_pivot_df = kpi_pivot_df.fillna('')
+
+        # Convertir las etiquetas de las columnas a enteros (los años)
+kpi_pivot_df.columns = kpi_pivot_df.columns.astype(int)
+
+        # Resetear el índice para llevar 'ESTACIONES' a una columna
+kpi_pivot_df.reset_index(inplace=True)
+
+        # Preparar los datos para el gráfico
+kpi_avg_by_country_station = filtered_df.groupby(['Pais', 'Estaciones'])['KPI'].mean().reset_index()
+
+        # Definir el esquema de color personalizado
+color_scheme = {
+        "Aprobacion": "lightgreen",
+        "Vigencia": "skyblue",
+        "PrimerDesembolso": "salmon",
+        "Elegibilidad": "gold"
+        }
+
+        # Crear el gráfico de barras apiladas
+bar_chart = alt.Chart(kpi_avg_by_country_station).mark_bar().encode(
+    x='Pais:N',
+    y=alt.Y('sum(KPI):Q', stack='zero', title='KPI Promedio'),
+    color=alt.Color('Estaciones:N', scale=alt.Scale(domain=list(color_scheme.keys()), range=list(color_scheme.values()))),
+    tooltip=['Pais', 'Estaciones', 'KPI']
 )
 
-plotting_demo()
+        # Crear las etiquetas de texto para cada barra
+text_chart = alt.Chart(kpi_avg_by_country_station).mark_text(
+    align='center',
+    baseline='middle',
+    color='black',  # Color del texto
+    ).encode(
+        x='Pais:N',
+        y=alt.Y('sum(KPI):Q', stack='zero', title=''),
+        text=alt.Text('sum(KPI):Q', format='.2f'),
+        color=alt.value('black')  # Esto asegura que el texto sea negro
+)
 
-show_code(plotting_demo)
+        # Combinar gráficos de barras y texto
+final_chart = (bar_chart + text_chart).properties(
+    width=600,
+    height=400,
+    title='KPI Promedio por País y Estación'
+)
+
+# Definir la paleta de colores para los países
+country_colors = {
+    "Argentina": "#36A9E1",
+    "Bolivia": "#F39200",
+    "Brasil": "#009640",
+    "Paraguay": "#E30613",
+    "Uruguay": "#27348B"
+    }
+
+# Crear el gráfico de pastel (pie chart)
+pie_chart = alt.Chart(filtered_df).transform_aggregate(
+    # Calcular el total de aportes por país
+    total='sum(AporteFONPLATAVigente)',
+    groupby=['Pais']
+).transform_calculate(
+    # Calcular el porcentaje del total para cada país
+    porcentaje='datum.total / sum(datum.total) over ()'
+).mark_arc().encode(
+    # Área del pastel basada en el porcentaje calculado
+    theta=alt.Theta(field='porcentaje', type='quantitative', stack=True),
+    # Color según el país con mapeo de colores personalizado
+    color=alt.Color('Pais:N', scale=alt.Scale(domain=list(country_colors.keys()), range=list(country_colors.values())), legend=alt.Legend(title="País")),
+    # Información para tooltip
+    tooltip=[alt.Tooltip('Pais:N'), alt.Tooltip('total:Q', title='Aporte FONPLATA Vigente')],
+    # Texto para la etiqueta, mostrando el país y el monto en millones formateado
+    text=alt.Text('Pais:N')
+).properties(
+    # Título del gráfico
+    title='Distribución de Aportes FONPLATA Vigentes por País'
+)
+
+# Mostrar el gráfico
+st.altair_chart(pie_chart, use_container_width=True)
+
